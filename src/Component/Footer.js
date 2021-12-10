3dc567026b1a58b6a6d5716ba2f8cf4a6de2bc21
onurakgl <73547559+Onurakgull@users.noreply.github.com>
2021-12-10 21:48:21 +0300

    Create Footer.js

---
+++ src/Component/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='flex justify-center bg-gray-800'>
+            <p className='text-center text-xl text-white'>Onur Akgül</p>
+        </div>
+    )
+}
+
+export default Footer
