f63ad4550a59630d51d0031d694fcfbabde49912
onurakgl <73547559+Onurakgull@users.noreply.github.com>
2021-12-10 21:48:19 +0300

    Update Main.js

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -3,17 +3,17 @@ import ImageOne from '../img/shopp.svg'
 
 const Main = () => {
     return (
-        <div className='flex flex-row justify-center mt-12'>
+        <div className='md:flex md:flex-row justify-center mt-12'>
             <div className="flex flex-col justify-center mr-7">
-                <h2 className='text-5xl font-serif text-green-800'>Merhaba Alışveriş Siteme Hoş Geldiniz!!!</h2>
+                <h2 className='text-5xl -mt-10 font-serif text-green-800 text-center md:self-start md:text-left'>Merhaba Alışveriş Siteme Hoş Geldiniz!!!</h2>
                 <hr className='text-gray-800'/>
-                <p className='text-xl uppercase text-gray-600'>Bu sitede istediğiniz ürünü kolay bir şekilde bulabilir ve alışverişin tadını çıkarabilirsiniz.</p>
-                <p className='text-xl uppercase text-gray-600'>Şimdi Alışverişe Başla!</p>
-                <button className='w-20 h-10 bg-red-400 rounded-full text-white text-sm hover:bg-red-600'>Now Run</button>
+                <p className='text-xl font-serif  text-gray-600 md:self-start' >Bu sitede istediğiniz ürünü kolay bir şekilde bulabilir ve alışverişin tadını çıkarabilirsiniz.</p>
+                <p className='text-xl font-serif  text-gray-600 md:self-start' >Şimdi Alışverişe Başla!</p>
+                <hr className='text-gray-800'/>
+                <button className='md:self-start py-4 px-8 my-5 bg-red-400 rounded-full text-white text-sm hover:bg-red-600'>Now Run</button>
             </div>
-            <div className=''>
-            <img src={ImageOne} 
-            name='edit-save' 
+            <div>
+            <img src={ImageOne} alt=""
             className='mb-4'/>
             </div>
         </div>
