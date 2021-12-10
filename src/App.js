a892428dc05465d5a3df53828645c8b798ec49c9
onurakgl <73547559+Onurakgull@users.noreply.github.com>
2021-12-10 21:48:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./Component/Footer";
 import Header from "./Component/Header";
 import Main from "./Component/Main";
 
@@ -6,7 +7,9 @@ function App() {
     <div className="container h-8 mx-auto p-5">
       <Header/>
       <Main/>
+      <Footer/>
     </div>
+    
   );
 }
 
