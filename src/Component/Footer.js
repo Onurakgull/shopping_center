1059353c0a19e5422c9e5cfaef724f79579d00c5
onurakgl <73547559+Onurakgull@users.noreply.github.com>
2021-12-11 17:36:02 +0300

    Update Footer.js

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='flex justify-center bg-gray-800'>
-            <p className='text-center text-xl text-white'>Onur Akgül</p>
+            <p className='text-center text-xl text-white'>@Onur Akgül</p>
         </div>
     )
 }
